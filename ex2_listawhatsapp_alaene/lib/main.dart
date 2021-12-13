9461fa2b0ed4575ece5a0db05ec4b01bebca8bcd
Alaene Sousa <Alaene.Sousa@ab-inbev.com>
2021-12-13 19:53:40 -0300

    asdasda
    asdasdasdasd

--- ex2_listawhatsapp_alaene/lib/main.dart
+++ ex2_listawhatsapp_alaene/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,19 +32,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.green.shade700,
-        elevation: 0,
-        centerTitle: true,
-        actions: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
-            ],
-          )
-        ],
-      ),
+          title: Text(widget.title),
+          backgroundColor: Colors.green.shade700,
+          elevation: 0,
+          centerTitle: true),
       body: Column(
         children: const <Widget>[
           TopBar(),
@@ -57,6 +47,84 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class TopBar extends StatelessWidget {
   const TopBar({Key? key}) : super(key: key);
+
+  static const listaContatos = [
+    {
+      'image': NetworkImage("https://loremflickr.com/320/320"),
+      'name': "Alaene",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/321"),
+      'name': "Tonny - My cat",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/322"),
+      'name': "Pedro - my boy",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/323"),
+      'name': "Aleatorio 1",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/324"),
+      'name': "Aleatorio 11",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/325"),
+      'name': "Aleatorio 111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/326"),
+      'name': "Aleatorio 1111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/327"),
+      'name': "Aleatorio 11111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/328"),
+      'name': "Aleatorio 11111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/329"),
+      'name': "Aleatorio 111111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/330"),
+      'name': "Alaene",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/331"),
+      'name': "Aleatorio 1111111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/332"),
+      'name': "Aleatorio 1111111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/333"),
+      'name': "Aleatorio 1111111",
+      'tel': "+ 55 31 9 94767115"
+    },
+    {
+      'image': NetworkImage("https://loremflickr.com/320/334"),
+      'name': "Aleatorio 1111111",
+      'tel': "+ 55 31 9 94767115"
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +146,46 @@ class TopBar extends StatelessWidget {
                 color: Colors.white),
             height: MediaQuery.of(context).size.height - 200,
           ),
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16, right: 10, bottom: 10),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height - 240,
+            child: ListView.builder(
+                itemCount: 15,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.lightGreen,
+                            backgroundImage: NetworkImage(
+                                "https://loremflickr.com/320/32$index"),
+                          )),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 160,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const <Widget>[
+                            Text(
+                              "Alaene <3",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+          ),
+        ),
       ],
     );
   }
