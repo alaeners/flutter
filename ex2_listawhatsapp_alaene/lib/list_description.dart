6ca06fd05b22044af9464db373634bd9166cbf2c
Alaene Sousa <Alaene.Sousa@ab-inbev.com>
2021-12-14 08:20:42 -0300

    asdasdsad
    sadsadasd

--- ex2_listawhatsapp_alaene/lib/list_description.dart
+++ ex2_listawhatsapp_alaene/lib/list_description.dart
@@ -1,2 +1,19 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
+
+class DescriptionContact extends StatelessWidget {
+  const DescriptionContact({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New Screen')),
+      body: Center(
+        child: Text(
+          'This is a new screen',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
